a37c96121d7516b2c960c0b3a8bdc7f06f7bb8c2
devnart <hr.fb001@gmail.com>
2021-02-18 09:48:34 +0100

    final

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-
 // Variables
-var basePrice = 0,fuel = 0,transPrice,totalPrice;
+var basePrice = 0,
+  fuel = 0,
+  transPrice,
+  totalPrice;
 var days = document.getElementById("days");
 
 // Calculate Total Price
@@ -11,34 +13,29 @@ boit.forEach((e) =>
   e.addEventListener("click", (f) => {
     fuel = e.value;
   })
-)
+);
 
-// get total days 
-
+// get total days
 
 // Calculate Total
-function calcPrice(){
-
-    console.log(days);
-    if (basePrice == 0) {
-      alert("Select Type !")
-    } else if(fuel == 0){
-      alert("Please Select Fuel Type !")
-    }else if(days.value == ""){
-      alert("Please Choose Days !")
-
-    }else  {
-      totalPrice = (basePrice+(basePrice*fuel)+(basePrice*transPrice))*days.value
-      console.log(totalPrice)
-      document.getElementById("totalPrice").innerHTML = totalPrice
-    }
-
+function calcPrice() {
+  console.log(days);
+  if (basePrice == 0) {
+    alert("Select Type !");
+  } else if (fuel == 0) {
+    alert("Please Select Fuel Type !");
+  } else if (days.value == "") {
+    alert("Please Choose Days !");
+  } else {
+    totalPrice =
+      (basePrice + basePrice * fuel + basePrice * transPrice) * days.value;
+    console.log(totalPrice);
+    document.getElementById("totalPrice").innerHTML = totalPrice;
+  }
 }
-
 
 // Process Selections (Type,Enable/Disbale Buttons)
 function getType() {
-
   var type = document.getElementById("type").value;
   switch (type) {
     case "Moto":
@@ -92,6 +89,7 @@ function getType() {
 
       basePrice = 16;
       transPrice = 0;
+      fuel = 0.21;
 
       break;
     case "Engin de Chantier":
@@ -117,11 +115,11 @@ function getType() {
 
       basePrice = 250;
       transPrice = 0.19;
+      fuel = 0.21;
 
       break;
   }
 }
-
 
 // Reset Buttons
 function clearList() {
@@ -136,54 +134,46 @@ function clearList() {
   document.getElementById("auto").checked = false;
   document.getElementById("manual").disabled = false;
   document.getElementById("auto").disabled = false;
-
 }
 
+// Hamburger Menu
 
-// Hamburger Menu 
+var navbar = document.querySelector(".ham_menu");
+var ham = document.querySelector(".ham");
 
-var navbar = document.querySelector(".ham_menu")
-var ham = document.querySelector(".ham")
+ham.addEventListener("click", toggleHamburger);
 
-ham.addEventListener("click", toggleHamburger)
-
-function toggleHamburger(){
-  navbar.classList.toggle("showNav")
-  ham.classList.toggle("showClose")
+function toggleHamburger() {
+  navbar.classList.toggle("showNav");
+  ham.classList.toggle("showClose");
 }
 
-// Mail Validation 
-function mailValidate(){
-  var name = document.querySelector("input[type='name']").value
-  var email = document.querySelector("input[type='email']").value
-  var tel = document.querySelector("input[type='tel']").value
-  var text = document.querySelector("input[type='text']").value
-  var message = document.getElementById("message").value
-  
-  // Modale 
-  var modal = document.querySelector(".modal")
-  var close = document.querySelector(".close")
+// Mail Validation
+function mailValidate() {
+  var name = document.querySelector("input[type='name']").value;
+  var email = document.querySelector("input[type='email']").value;
+  var tel = document.querySelector("input[type='tel']").value;
+  var text = document.querySelector("input[type='text']").value;
+  var message = document.getElementById("message").value;
 
+  // Modale
+  var modal = document.querySelector(".modal");
+  var close = document.querySelector(".close");
 
-  if ( name == "" || email == "" || tel == "" || text == "" || message == "") {
-    alert("Please Fill All Informations")
-    
-  }else {
-    modal.style.transform = "translateY(0)"
+  if (name == "" || email == "" || tel == "" || text == "" || message == "") {
+    alert("Please Fill All Informations");
+  } else {
+    modal.style.transform = "translateY(0)";
+    document.querySelector('body').style.overflowY = "hidden";
 
   }
-  close.addEventListener("click",()=> {
-    modal.style.transform = "translateY(-100%)"
-  })
-
-
+  close.addEventListener("click", () => {
+    document.querySelector('body').style.overflowY = "auto";
+    modal.style.transform = "translateY(-100%)";
+  });
 
   document.querySelector("#name").innerHTML = name;
   document.querySelector("#email").innerHTML = email;
   document.querySelector("#number").innerHTML = tel;
   document.querySelector("#msg").innerHTML = message;
-
-  
 }
-
-
